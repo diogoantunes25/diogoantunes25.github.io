bfe57053654683b401edaa994d22b82e5760ad1e
Diogo Antunes <diogo.santiago.pt@gmail.com>
2021-12-21 06:21:59 +0000

    Basic obstacle generator mechanics

--- scripts/game1.js
+++ scripts/game1.js
@@ -1,8 +1,25 @@
+// TODO: Put correct info
+const OBSTACLES = [
+    {name: "blank", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "carlao", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "stephanie", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "casaLina", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "exame", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "metro", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "shot", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "boti", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+    {name: "montanha-russa", question: "O que fazes?", correctAnswer: "", wrongAnswer: ""},
+];
+
+const MIN_TIME = 500; // 0.5 seconds between obstacles (minimum)
+const TIME_DELTA = 3000; // 0.5 to 0.5 + 3 seconds between obstacles
+
 const pegCharacter = document.getElementById("peg-character");
 const obstacles = document.querySelectorAll(".obstacle");
+let notColidedObstacles = [];
+obstacles.forEach((obs) => {notColidedObstacles.push(obs)});
 
-setInterval(() => {
-        console.log("HI");
+setTimeout(() => {
         obstacles.forEach((obstacle) => {
             obstacle.classList.add("moved");
         })
@@ -11,9 +28,52 @@ setInterval(() => {
 
 const startJump = (peg) => { peg.classList.add("high"); }
 const stopJump = (peg) => { peg.classList.remove("high"); }
-const areColiding = (el1, el2) => {
-    // Use offsets to check if there is a collision
+
+setInterval(() => {
+    notColidedObstacles.forEach( (obstacle) => {
+        if (areColiding(pegCharacter, obstacle)) {
+            notColidedObstacles = notColidedObstacles.filter((obs) => obs != obstacle);
+            console.log("collision");
+        }
+    })}
+, 200);
+
+
+const areColiding = (a, b) => {
+    var aRect = a.getBoundingClientRect();
+    var bRect = b.getBoundingClientRect();
+
+    return !(
+        ((aRect.top + aRect.height) < (bRect.top)) ||
+        (aRect.top > (bRect.top + bRect.height)) ||
+        ((aRect.left + aRect.width) < bRect.left) ||
+        (aRect.left > (bRect.left + bRect.width))
+    );
 }
+
+const removeLife = () => {
+};
+
+function obstacle () {
+    const i = Math.floor(Math.random() * 8);
+    const newObstacle = document.createElement("div");
+    const obstacleList = document.querySelector(".obstacles-container");
+    notColidedObstacles.push(newObstacle);
+    obstacleList.appendChild(newObstacle);
+    newObstacle.classList.add(`obstacle-${i}`);
+    newObstacle.classList.add(`obstacle`);
+    setTimeout(() => {
+        newObstacle.classList.add("moved");
+    }, 2);
+
+    setTimeout(() => {
+        newObstacle.remove();
+        notColidedObstacles = notColidedObstacles.filter((obs) => obs != newObstacle);
+    }, 30000);
+
+
+    return newObstacle;
+} 
 
 window.addEventListener("space", () => (jump(pegCharacter)));
 window.addEventListener("touchstart", () => {
@@ -22,6 +82,11 @@ window.addEventListener("touchstart", () => {
     }
 );
 
+const newTimeout = () => {
+    setTimeout(() => {
+        obstacle();
+        newTimeout();
+    }, MIN_TIME + Math.floor(Math.random() * TIME_DELTA));
+}
 
-
-
+newTimeout();
